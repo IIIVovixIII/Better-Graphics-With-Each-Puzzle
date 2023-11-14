fb04bbe5d6ca0fd421c4b4181cf5f5e1d1d08910
IIIVovixIII <brandon.bourgoin@mymail.champlain.edu>
2023-11-14 16:51:05 -0500

    Fixed Some Bugs

---
+++ Better Graphics with each puzzle/Assets/Scripts/StartButton.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Sceneloader : MonoBehaviour
+{
+
+    public void loadscene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
