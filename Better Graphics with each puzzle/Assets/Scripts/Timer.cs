02671a72b04bd381b7d7e853db995cddf09fbe1c
anthoy333 <54159279+anthoy333@users.noreply.github.com>
2023-11-30 17:03:17 -0500

    I finally got the stupid high score code working. It only took me around 3 hours.

--- Better Graphics with each puzzle/Assets/Scripts/Timer.cs
+++ Better Graphics with each puzzle/Assets/Scripts/Timer.cs
@@ -5,10 +5,16 @@ using UnityEngine.SceneManagement; // Import this for scene management
 public class Timer : MonoBehaviour
 {
     public TextMeshProUGUI timerText; // Reference to the TextMeshPro text object
-    public TextMeshProUGUI highScoreText; // Reference to display the high score
+    public TextMeshProUGUI bestTimeText; // Reference to display the best time
+
+    public bool[] levelsCompleted = new bool[2]; // Array to track the completion of levels 1-4
+    public string[] levels = new string[2] { "Level 1", "Level 2"}; // Update with actual level names
 
     private const string timerKey = "MyTimer";
-    private const string highScoreKey = "HighScore";
+    private const string bestTimeKey = "BestTime"; // Changed from HighScoreKey to BestTimeKey
+    private const string levelCompletedKey = "LevelCompleted"; // Key prefix for level completion
+    public float bestTime = 0;
+
     public float timer;
     private bool isTimerRunning = true; // Flag to check if the timer is running
 
@@ -16,27 +22,69 @@ public class Timer : MonoBehaviour
     {
         // Load the timer from PlayerPrefs when the scene starts
         timer = PlayerPrefs.GetFloat(timerKey, 0f);
+        LoadLevelCompletions();
         Debug.Log("Current Timer: " + timer);
-
+        if (SceneManager.GetActiveScene().name == "Level 1")
+        {
+            ResetLevelCompletions();
+            SaveLevelCompletions();
+            ResetTimer();
+        }
         // Start counting time
         InvokeRepeating("IncreaseTimer", 1f, 1f);
     }
 
     void Update()
     {
+        string currentSceneName = SceneManager.GetActiveScene().name;
         // Check if the current scene is Main Menu
         if (SceneManager.GetActiveScene().name == "Main Menu")
         {
-            StopTimerAndCheckHighScore();
+            StopTimerAndCheckBestTime();
+        }
+        for (int i = 0; i < levels.Length; i++)
+        {
+            if (currentSceneName == levels[i])
+            {
+                levelsCompleted[i] = true;
+                PlayerPrefs.SetInt(levelCompletedKey + i, 1); // Save level completion
+                PlayerPrefs.Save();
+            }
+        }
+    }
+    private void SaveLevelCompletions()
+    {
+        for (int i = 0; i < levels.Length; i++)
+        {
+            PlayerPrefs.SetInt(levelCompletedKey + i, levelsCompleted[i] ? 1 : 0);
+        }
+        PlayerPrefs.Save();
+    }
+    private void LoadLevelCompletions()
+    {
+        for (int i = 0; i < levels.Length; i++)
+        {
+            levelsCompleted[i] = PlayerPrefs.GetInt(levelCompletedKey + i, 0) == 1;
         }
     }
 
+    private bool AreAllLevelsCompleted()
+    {
+        LoadLevelCompletions(); // Make sure to load the latest data
+        foreach (bool levelCompleted in levelsCompleted)
+        {
+            if (!levelCompleted) return false;
+        }
+        return true;
+    }
     void IncreaseTimer()
     {
         if (isTimerRunning)
         {
             timer++;
             UpdateTimerText(); // Update the timer text on each increment
+            PlayerPrefs.SetFloat(timerKey, timer);
+            PlayerPrefs.Save();
         }
     }
 
@@ -49,33 +97,50 @@ public class Timer : MonoBehaviour
         }
     }
 
-    public void StopTimerAndCheckHighScore()
+    public void StopTimerAndCheckBestTime()
     {
         isTimerRunning = false; // Stop the timer
+        Debug.Log("Option1");
 
-        float highScore = PlayerPrefs.GetFloat(highScoreKey, float.MaxValue);
-
-        if (timer < highScore) // Check if the current timer is a new high score
+        // Check if all levels are completed
+        if (AreAllLevelsCompleted())
         {
-            PlayerPrefs.SetFloat(highScoreKey, timer);
-            PlayerPrefs.Save();
-            if (highScoreText != null)
-            {
-                highScoreText.text = "High Score: " + timer.ToString("F0");
-            }
-            Debug.Log("New High Score: " + timer);
-        }
+            bestTime = bestTime;
+            Debug.Log(bestTime);
+            Debug.Log("All levels completed. Current timer: " + timer + ", Best time: " + bestTime);
 
-        // Reset the timer for the next game
-        ResetTimer();
+            // Check if the current timer is a new best time and it's not zero
+            if(bestTime == 0)
+            {
+                bestTime = timer;
+                PlayerPrefs.SetFloat(bestTimeKey, timer);
+                PlayerPrefs.Save();
+                bestTimeText.text = "Best Time: " + timer.ToString("F0");
+            }
+            if (timer < bestTime)
+            { 
+                PlayerPrefs.SetFloat(bestTimeKey, timer);
+                PlayerPrefs.Save();
+                bestTimeText.text = "Best Time: " + timer.ToString("F0");
+                timerText.text = "Your Time: " + timer.ToString("F0"); // F0 for displaying whole numbers
+
+                Debug.Log("New Best Time: " + timer);
+            }
+        }
+        else
+        {
+            //Debug.Log("Not all levels completed. Best time not updated.");
+        }
     }
+
+
 
     void ResetTimer()
     {
         timer = 0f;
         PlayerPrefs.SetFloat(timerKey, timer);
         PlayerPrefs.Save();
-        Debug.Log("Timer reset to zero.");
+        //Debug.Log("Timer reset to zero.");
     }
 
     // Call this method when starting a new game
@@ -83,5 +148,12 @@ public class Timer : MonoBehaviour
     {
         isTimerRunning = true;
         ResetTimer();
+    }
+    private void ResetLevelCompletions()
+    {
+        for (int i = 0; i < levelsCompleted.Length; i++)
+        {
+            levelsCompleted[i] = false;
+        }
     }
 }
