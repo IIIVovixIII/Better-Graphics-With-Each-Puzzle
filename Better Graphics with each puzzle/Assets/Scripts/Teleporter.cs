1e11b3fa9b1c733eaa849132fdd290c4a3962d3d
anthoy333 <54159279+anthoy333@users.noreply.github.com>
2023-12-01 12:43:23 -0500

    added Teleporter.cs

---
+++ Better Graphics with each puzzle/Assets/Scripts/Teleporter.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    // Reference to the destination teleportation platform
+    public Transform destinationPlatform;
+
+    // Called when a player enters the platform trigger collider
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Check if the object entering the trigger is the player (you can customize the tag or layer)
+        if (other.CompareTag("Player"))
+        {
+            // Teleport the player to the destination platform
+            TeleportPlayer(other.transform);
+        }
+    }
+
+    // Teleport the player to the destination platform
+    private void TeleportPlayer(Transform playerTransform)
+    {
+        // Set the player's position to the destination platform's position
+        playerTransform.position = destinationPlatform.position + 5;
+
+        // Optionally, add any extra effects or animations here
+    }
+}
