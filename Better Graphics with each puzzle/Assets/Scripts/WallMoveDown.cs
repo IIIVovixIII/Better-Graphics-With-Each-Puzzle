59c7e3107135997fbff7108779f54b4dd42aecf7
anthoy333 <54159279+anthoy333@users.noreply.github.com>
2023-12-06 18:02:51 -0500

    I redid level 1

---
+++ Better Graphics with each puzzle/Assets/Scripts/WallMoveDown.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallMoveDown : MonoBehaviour
+{
+    public float moveSpeed = 15.0f;
+    public float moveDistance = 10.0f; // Adjust this to control how far it moves
+    public GameObject wall;
+    private bool movingDown;
+    private Vector3 initialPosition;
+
+    void Start()
+    {
+        initialPosition = wall.transform.position;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if the collision is with a GameObject tagged "Player"
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("hi");
+            movingDown = true;
+        }
+    }
+
+    void Update()
+    {
+        if (movingDown)
+        {
+            // Move the wall down
+            wall.transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+
+            // Check if the wall has moved the desired distance
+            if (Vector3.Distance(initialPosition, wall.transform.position) >= moveDistance)
+            {
+                movingDown = false;
+            }
+        }
+    }
+}
